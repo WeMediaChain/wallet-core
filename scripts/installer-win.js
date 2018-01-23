c5e736e23dd641e0156d7e11f10d2bdcf750e8d9
MiRinZhang <noodles@yabing.me>
2018-01-23 12:35:33 +0800

    test release win

--- scripts/installer-win.js
+++ scripts/installer-win.js
@@ -1,22 +1,40 @@
 const { createWindowsInstaller } = require('electron-winstaller'),
+    { MSICreator } = require('electron-wix-msi'),
     fs = require('fs'),
+    pkg = require('../package'),
     OUT_PUT = './release';
 
 const winOptions = {
-    appDirectory: 'build/WMCWallet-win32-x64',
-    iconUrl: 'https://nzsjtzs2b.qnssl.com/icon.ico',
-    setupIcon: 'icons/icon.ico',
-    outputDirectory: OUT_PUT,
-    exe: 'WMCWallet.exe',
-};
+        appDirectory: 'build/WMCWallet-win32-x64',
+        iconUrl: 'https://nzsjtzs2b.qnssl.com/icon.ico',
+        setupIcon: 'icons/icon.ico',
+        outputDirectory: OUT_PUT,
+        exe: 'WMCWallet.exe',
+    },
+    msiOptions = {
+        appDirectory: 'build/WMCWallet-win32-x64',
+        outputDirectory: OUT_PUT,
+        description: pkg.description,
+        exe: pkg.name,
+        name: pkg.name,
+        manufacturer: 'We Media Chain Wallet',
+        version: pkg.version,
+    };
 
 
 if (!fs.existsSync(OUT_PUT)) {
     fs.mkdirSync(OUT_PUT);
 }
 
-createWindowsInstaller(winOptions).then(_ => {
-    console.log('创建安装文件成功');
-}, err => {
-    console.error('创建安装文件失败', err);
-});
+(async () => {
+    const msiCreator = new MSICreator(msiOptions);
+    
+    await msiCreator.create();
+    await msiCreator.compile();
+})();
+
+// createWindowsInstaller(winOptions).then(_ => {
+//     console.log('创建安装文件成功');
+// }, err => {
+//     console.error('创建安装文件失败', err);
+// });
